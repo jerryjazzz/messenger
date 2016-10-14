fbcf622701b9869b0f484bca4406e2240d597662
Taimur Naziri <taimurknaziri@gmail.com>
2016-10-14 11:00:27 -0400

    Resolved error in routes file.

--- client/scripts/routes.js
+++ client/scripts/routes.js
@@ -2,20 +2,22 @@ import {Config} from 'angular-ecmascript/module-helpers';
 
 export default class RoutesConfig extends Config {
   configure() {
-  	this.$stateProvider.state('tab', {
-			url: '/tab',
-			abstract: true,
-			templateUrl: 'client/templates/tabs.html'
-		})
-		.state('tab.chats', {
-			url: '/chats',
-			views: {
-				'tab-chats': {
-					templateUrl: 'client/templates/chats.html'
+  	this.$stateProvider
+	  	.state('tab', {
+				url: '/tab',
+				abstract: true,
+				templateUrl: 'client/templates/tabs.html'
+			})
+			.state('tab.chats', {
+				url: '/chats',
+				views: {
+					'tab-chats': {
+						templateUrl: 'client/templates/chats.html'
+					}
 				}
-			}
-		});
-		this.$urlRouteProvider.otherwise('tab/chats');
+			});
+
+		this.$urlRouterProvider.otherwise('tab/chats');
   }
 }
 
